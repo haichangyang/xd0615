91367f658bd4ea2e8e65f447fcb4a51b8d64091a
Your Name <you@example.com>
2020-06-21 13:16:48 +0800

    作业更改

--- lesson5/1.ino
+++ lesson5/1.ino
@@ -1,4 +1,3 @@
-
 #include <LiquidCrystal.h>
 
 
@@ -7,13 +6,21 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
 
   lcd.begin(16, 2);
-
-  lcd.print("That is a nice day");
+  lcd.setCursor(0,0);
+  lcd.print("Start");
+  
+  Serial.begin(9600);
 }
 
-void loop() {
-
-  lcd.setCursor(1, 1);
-  lcd.print(millis() / 1000);
+void loop() 
+{
+  if(Serial.available()>0)
+  {
+    
+    lcd.setCursor(0,1);
+    lcd.print(Serial.readString());
+    
+  }
+ 
 }
  
